8b70e1039158b0ff1845b916f81fb5acc543964d
SMNAFI <sayem.md.nafi@gmail.com>
2021-06-04 21:46:49 +0600

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import CarItem from './components/CarItem';
+import CarsList from './components/CarList';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Header />
+
+      <CarsList />
+
       <StatusBar style="auto" />
     </View>
   );
---
+++ components/CarList/cars.js
@@ -0,0 +1,40 @@
+export default [{
+    name: 'Model S',
+    tagline: 'Starting at $69,420',
+    image: require('../../assets/images/ModelS.jpeg'),
+    range: '387 to 520 mi battery-only',
+    price: 'From $69,420',
+    horsepower: '670 to 1,020 hp',
+    dimensions: '196″ L x 77″ W x 57″ H',
+    weight: '4,561 to 4,941 lbs'
+}, {
+    name: 'Model 3',
+    tagline: 'Order Online for',
+    taglineCTA: 'Touchless Delivery',
+    image: require('../../assets/images/Model3.jpeg'),
+    range: '263 to 353 mi battery-only',
+    price: 'From $39,990',
+    horsepower: 'Up to 150 city / 133 highway',
+    dimensions: '185″ L x 73″ W x 56-57″ H',
+    weight: '8.5 to 10h at 220V'
+}, {
+    name: 'Model X',
+    tagline: 'Order Online for',
+    taglineCTA: 'Touchless Delivery',
+    image: require('../../assets/images/ModelX.jpeg'),
+    range: '340 to 371 mi battery-only',
+    price: 'From $79,990',
+    horsepower: '670 to 1,020 hp',
+    dimensions: '198″ L x 79″ W x 66″ H',
+    weight: '5,185 to 5,648 lbs'
+}, {
+    name: 'Model Y',
+    tagline: 'Order Online for',
+    taglineCTA: 'Touchless Delivery',
+    image: require('../../assets/images/ModelY.jpeg'),
+    range: '244 to 326 mi battery-only',
+    price: 'From $39,990',
+    horsepower: '68 ft³ with seat area',
+    dimensions: '196″ L x 77″ W x 57″ H',
+    weight: '4,416 lbs'
+}];
